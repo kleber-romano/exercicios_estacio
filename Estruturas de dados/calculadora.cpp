0d62d7d9bdb665a925b13b7e0a0de861c53fb7ac
Kleber Romano de Sousa <kleber.romano@outlook.com>
2020-05-22 09:48:15 -0300

    Prototipo de calculadora com as 4 operacoes basicas

--- Estruturas de dados/calculadora.cpp
+++ Estruturas de dados/calculadora.cpp
@@ -6,9 +6,67 @@ using std::cout;
 using std::endl;
 using std::cin;
 
+void separador()
+{
+	int x;
+
+	for(int x=1; x<=30; x++)
+	cout << "*";
+}
 int main()
 {
-	cout << "Hello World! \n\n";
+	//Definindo a localização
+	setlocale(LC_ALL, "");
+	
+	//Variáveis
+	float num1, num2, soma, subt, mult, divis;
+	char operador;
+
+	//Entrada de dados
+	cout << "\n\n";
+	cout << "\n\n********** CALCULADORA **********\n\n";
+	
+	separador();
+	cout << "\nDigite com o primeiro número: ";
+	cin >> num1;
+
+	cout << "Digite o segundo número: ";
+	cin >> num2;
+
+	//Definindo operações matemáticas
+	soma = num1 + num2;
+	subt = num1 - num2;
+	mult = num1 * num2;
+	divis = num1 / num2;
+
+	cout << "\n";
+	separador();
+
+	cout << "\nSelecione a operação desejada: \n";
+	cout << "(+) SOMA \n";
+	cout << "(-) SUBTRAÇÃO \n";
+	cout << "(*) MULTIPLICAÇÃO \n";
+	cout << "(/) DIVISÃO \n";
+	cin >> operador;
+
+	switch (operador)
+	{
+		case '+': cout << soma << endl;
+		break;
+
+		case '-': cout << subt << endl;
+		break;
+
+		case '*': cout << mult << endl;
+		break;
+
+		case '/': cout << divis << endl;
+		break;
+
+		default: cout << "A operação escolhida não pode ser realizada. Execute o programa novamente.";
+	}
+	separador();
+	cout << "\n\n";
 	
 	return 0;
 }
